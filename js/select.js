aed59ca77fc3271ad8e9b095f11a15811cbbbe2b
maisha12 <62903120+maisha12@users.noreply.github.com>
2022-08-22 01:21:42 +0600

    select five dine

---
+++ js/select.js
@@ -0,0 +1,42 @@
+document.getElementById('btn-select').addEventListener('click', function () {
+    const nameFeild = document.getElementById('image1-name');
+    const nameOne = nameFeild.innerText;
+    const firstClickedElement = document.getElementById('first-clicked')
+    const firstClicked = firstClickedElement.innerText;
+    firstClickedElement.innerText = nameOne;
+})
+document.getElementById('btn-select-one').addEventListener('click', function () {
+    const nameFeild = document.getElementById('image2-name');
+    const nameOne = nameFeild.innerText;
+    const firstClickedElement = document.getElementById('second-clicked')
+    const firstClicked = firstClickedElement.innerText;
+    firstClickedElement.innerText = nameOne;
+})
+document.getElementById('btn-select-two').addEventListener('click', function () {
+    const nameFeild = document.getElementById('image3-name');
+    const nameOne = nameFeild.innerText;
+    const firstClickedElement = document.getElementById('third-clicked')
+    const firstClicked = firstClickedElement.innerText;
+    firstClickedElement.innerText = nameOne;
+})
+document.getElementById('btn-select-three').addEventListener('click', function () {
+    const nameFeild = document.getElementById('image4-name');
+    const nameOne = nameFeild.innerText;
+    const firstClickedElement = document.getElementById('fourth-clicked')
+    const firstClicked = firstClickedElement.innerText;
+    firstClickedElement.innerText = nameOne;
+})
+document.getElementById('btn-select-four').addEventListener('click', function () {
+    const nameFeild = document.getElementById('image5-name');
+    const nameOne = nameFeild.innerText;
+    const firstClickedElement = document.getElementById('five-clicked')
+    const firstClicked = firstClickedElement.innerText;
+    firstClickedElement.innerText = nameOne;
+})
+document.getElementById('btn-select-five').addEventListener('click', function () {
+    const nameFeild = document.getElementById('image6-name');
+    const nameOne = nameFeild.innerText;
+    const firstClickedElement = document.getElementById('first-clicked')
+    const firstClicked = firstClickedElement.innerText;
+    firstClickedElement.innerText = nameOne;
+})
