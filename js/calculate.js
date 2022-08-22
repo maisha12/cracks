88d03eeab94982c4b7ac10ba5339ed8f557b3c64
maisha12 <62903120+maisha12@users.noreply.github.com>
2022-08-22 20:53:55 +0600

    tittle added

--- js/calculate.js
+++ js/calculate.js
@@ -1,8 +1,2 @@
-document.getElementById('expenses-calculate').addEventListener('click', function () {
-    const perPlayerFeild = document.getElementById('input-perplayer');
-    const perPlayerString = perPlayerFeild.value;
-    const previousPerPlayer = parseInt(perPlayerString);
 
-
-})
 
