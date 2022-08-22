23d820c7c8ab9a8570fd758e9de69cf0feddbc14
maisha12 <62903120+maisha12@users.noreply.github.com>
2022-08-22 19:32:34 +0600

    player expenses added

---
+++ js/calculate.js
@@ -0,0 +1,8 @@
+document.getElementById('expenses-calculate').addEventListener('click', function () {
+    const perPlayerFeild = document.getElementById('input-perplayer');
+    const perPlayerString = perPlayerFeild.value;
+    const previousPerPlayer = parseInt(perPlayerString);
+
+
+})
+
--- js/select.js
+++ js/select.js
@@ -1,42 +1,128 @@
+let clickCount = 0;
 document.getElementById('btn-select').addEventListener('click', function () {
+    clickCount++;
+    if (clickCount > 5) {
+        alert('CANT ADD');
+        return;
+    }
     const nameFeild = document.getElementById('image1-name');
     const nameOne = nameFeild.innerText;
-    const firstClickedElement = document.getElementById('first-clicked')
-    const firstClicked = firstClickedElement.innerText;
-    firstClickedElement.innerText = nameOne;
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(nameOne);
+    node.appendChild(textnode);
+    const list = document.getElementById('clicked');
+    node.classList.add('list-group-item');
+    list.appendChild(node);
+    document.getElementById('btn-select').setAttribute('disabled', 'disabled');
+
+
 })
 document.getElementById('btn-select-one').addEventListener('click', function () {
+    clickCount++;
+    if (clickCount > 5) {
+        alert('CANT ADD');
+        return;
+    }
     const nameFeild = document.getElementById('image2-name');
     const nameOne = nameFeild.innerText;
-    const firstClickedElement = document.getElementById('second-clicked')
-    const firstClicked = firstClickedElement.innerText;
-    firstClickedElement.innerText = nameOne;
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(nameOne);
+    node.appendChild(textnode);
+    const list = document.getElementById('clicked');
+    node.classList.add('list-group-item');
+    list.appendChild(node);
+    document.getElementById('btn-select-one').setAttribute('disabled', 'disabled');
+
+
 })
 document.getElementById('btn-select-two').addEventListener('click', function () {
+    clickCount++;
+    if (clickCount > 5) {
+        alert('CANT ADD');
+        return;
+    }
     const nameFeild = document.getElementById('image3-name');
     const nameOne = nameFeild.innerText;
-    const firstClickedElement = document.getElementById('third-clicked')
-    const firstClicked = firstClickedElement.innerText;
-    firstClickedElement.innerText = nameOne;
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(nameOne);
+    node.appendChild(textnode);
+    const list = document.getElementById('clicked');
+    node.classList.add('list-group-item');
+    list.appendChild(node);
+    document.getElementById('btn-select-two').setAttribute('disabled', 'disabled');
+
+
 })
 document.getElementById('btn-select-three').addEventListener('click', function () {
+    clickCount++;
+    if (clickCount > 5) {
+        alert('CANT ADD');
+        return;
+    }
     const nameFeild = document.getElementById('image4-name');
     const nameOne = nameFeild.innerText;
-    const firstClickedElement = document.getElementById('fourth-clicked')
-    const firstClicked = firstClickedElement.innerText;
-    firstClickedElement.innerText = nameOne;
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(nameOne);
+    node.appendChild(textnode);
+    const list = document.getElementById('clicked');
+    node.classList.add('list-group-item');
+    list.appendChild(node);
+    document.getElementById('btn-select-three').setAttribute('disabled', 'disabled');
+
+
 })
 document.getElementById('btn-select-four').addEventListener('click', function () {
+    clickCount++;
+    if (clickCount > 5) {
+        alert('CANT ADD');
+        return;
+    }
     const nameFeild = document.getElementById('image5-name');
     const nameOne = nameFeild.innerText;
-    const firstClickedElement = document.getElementById('five-clicked')
-    const firstClicked = firstClickedElement.innerText;
-    firstClickedElement.innerText = nameOne;
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(nameOne);
+    node.appendChild(textnode);
+    const list = document.getElementById('clicked');
+    node.classList.add('list-group-item');
+    list.appendChild(node);
+    document.getElementById('btn-select-four').setAttribute('disabled', 'disabled');
+
+
 })
 document.getElementById('btn-select-five').addEventListener('click', function () {
+    clickCount++;
+    if (clickCount > 5) {
+        alert('CANT ADD');
+        return;
+    }
     const nameFeild = document.getElementById('image6-name');
     const nameOne = nameFeild.innerText;
-    const firstClickedElement = document.getElementById('first-clicked')
-    const firstClicked = firstClickedElement.innerText;
-    firstClickedElement.innerText = nameOne;
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(nameOne);
+    node.appendChild(textnode);
+    const list = document.getElementById('clicked');
+    node.classList.add('list-group-item');
+    list.appendChild(node);
+    document.getElementById('btn-select-five').setAttribute('disabled', 'disabled');
+
 })
+
+document.getElementById('expenses-calculate').addEventListener('click', function () {
+    const perPlayerFeild = document.getElementById('input-perplayer');
+    const perPlayerString = perPlayerFeild.value;
+    const perPlayerValue = parseFloat(perPlayerString);
+    const listParent = document.getElementById('clicked');
+    const listItemCount = listParent.children.length;
+    const playerCost = perPlayerValue * listItemCount;
+    const playerExpenses = document.getElementById('pE');
+    playerExpenses.innerText = playerCost;
+
+})
+document.getElementById('calTotal').addEventListener('click', function () {
+    const totalExpenses = pE.innerText;
+
+
+})
+
+
+
